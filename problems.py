bd155f9160bea3975ccbb49f6f1439b21193745a
Bennett Parsons <bpdman@gmail.com>
2017-12-04 15:20:33 -0500

    more chorus output

--- problems.py
+++ problems.py
@@ -72,7 +72,8 @@ if __name__ == "__main__":
 
 	numerals = [('I','7')]*4 + [('IV','7')]*2 + [('I','7')]*2 + [('V','7'), ('IV','7'), ('I','7'), ('V','7')]
 	progression = util.build_progression('C', numerals)
-	util.write_midi(solo=problem(progression, choruses=2, res_chord=util.build_chord('C', 'I', '7')).get_solo(), chords=progression*2 + [util.build_chord('C', 'I', '7')])
+	num_choruses = 4
+	util.write_midi(solo=problem(progression, choruses=num_choruses, res_chord=util.build_chord('C', 'I', '7')).get_solo(), chords=progression*num_choruses + [util.build_chord('C', 'I', '7')])
 
 
 
